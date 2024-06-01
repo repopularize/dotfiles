74610e9d6e4827ba8421bc18e98011263f14f4d3
yosoof3 <boxicalcube@proton.me>
2024-06-01 15:19:53 -0400

    fix: import options

--- modules/base/default.nix
+++ modules/base/default.nix
@@ -3,5 +3,6 @@
     ./environment # environment stuff (eg. fonts, and more if needed)
     ./nix # nix package manager options
     ./users # users of the machine
+    ./options # machine options
   ];
 }
