bb661f4dc9050f23cd6d5e5d455f94cc1154baae
yosoof3 <boxicalcube@proton.me>
2024-05-31 22:27:30 -0400

    feat: output nixos and home manager modules

--- parts/default.nix
+++ parts/default.nix
@@ -1,5 +1,7 @@
 {
   imports = [
     ./systems.nix # system architectures for these dotfiles
+
+    ./modules # outputed nixos and home-manager modules
   ];
 }
---
+++ parts/modules/default.nix
@@ -0,0 +1,22 @@
+{ self, ... }:
+let
+  mkModule =
+    path: if builtins.isPath path then self + path else builtins.throw "${path} does not exist";
+in
+{
+  flake = {
+    nixosModules = {
+      # i do not provide a default module, so throw an error
+      default = builtins.throw "There is no default module.";
+    };
+
+    # Currently there are no darwin modules
+    darwinModules = {
+      default = builtins.throw "There is no default module.";
+    };
+
+    homeManagerModules = {
+      default = builtins.throw "There is no default module.";
+    };
+  };
+}
