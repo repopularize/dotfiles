982b02cb23efbc462bad1f23b1c0c0489a7dae5a
yosoof3 <boxicalcube@proton.me>
2024-06-01 11:46:04 -0400

    refactor: remove darwin support

--- parts/lib/hardware.nix
+++ parts/lib/hardware.nix
@@ -1,21 +1,5 @@
 _:
 let
-  # check if the host platform is linux and x86
-  # (isx86Linux pkgs) -> true
-  isx86Linux = pkgs: with pkgs.stdenv; hostPlatform.isLinux && hostPlatform.isx86;
-
-  # ldTernary, short for linux darwin ternary, is a ternary operator that takes 3 arguments
-  # the pkgs used to determine the standard environment, l: the linux result, d: the darwin result
-  # https://github.com/nekowinston/dotfiles/blob/36f7f4a86af4d1ecd3d2da483585e4d2503a978a/machines/lib.nix#L32
-  ldTernary =
-    pkgs: l: d:
-    if pkgs.stdenv.isLinux then
-      l
-    else if pkgs.stdenv.isDarwin then
-      d
-    else
-      throw "Unsupported system: ${pkgs.stdenv.system}";
-
   # assume the first monitor in the list of monitors is primary
   # get its name from the list of monitors
   # `primaryMonitor osConfig` -> "DP-1"
