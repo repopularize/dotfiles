fa4541fe5c6ab22fab74357b785a6180b2bd912f
yosoof3 <boxicalcube@proton.me>
2024-06-01 17:52:39 -0400

    fix: remove wine because I removed it from options anyway
    woopsie

--- modules/profiles/meta/graphical/programs/cli.nix
+++ modules/profiles/meta/graphical/programs/cli.nix
@@ -23,13 +23,4 @@
       loadInNixShell = true;
     };
   };
-
-  # determine which version of wine to use
-  environment.systemPackages = with pkgs;
-    let
-      winePackage = if (lib.isWayland config) then
-        wineWowPackages.waylandFull
-      else
-        wineWowPackages.stableFull;
-    in lib.mkIf config.modules.programs.agnostic.wine.enable [ winePackage ];
 }
