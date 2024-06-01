06c5f5bc5f92f661c9a6dc9114cc7e663fd0130d
yosoof3 <boxicalcube@proton.me>
2024-05-31 22:30:39 -0400

    feat: lib for flake-parts
    credit to @isabelroses on github for all this code

--- parts/default.nix
+++ parts/default.nix
@@ -3,5 +3,6 @@
     ./systems.nix # system architectures for these dotfiles
 
     ./modules # outputed nixos and home-manager modules
+    ./lib # the lib that is used throughout system and home
   ];
 }
---
+++ parts/lib/import.nix
@@ -0,0 +1,38 @@
+{ inputs }:
+let
+  inherit (inputs.nixpkgs) lib;
+  inherit (lib) foldl recursiveUpdate;
+
+  # wrap the import with a pre-inherited lib to avoid typing it over and over again
+  # credits to @nrabulinski
+  import' =
+    path:
+    let
+      func = import path;
+      args = lib.functionArgs func;
+      requiredArgs = lib.filterAttrs (_: val: !val) args;
+      defaultArgs = (lib.mapAttrs (_: _: null) requiredArgs) // {
+        inherit lib;
+      };
+      functor = {
+        __functor = _: attrs: func (defaultArgs // attrs);
+      };
+    in
+    (func defaultArgs) // functor;
+
+  builders = import' ./builders.nix { inherit inputs; };
+  services = import' ./services.nix;
+  validators = import' ./validators.nix;
+  helpers = import' ./helpers.nix;
+  hardware = import' ./hardware.nix;
+
+  # recursively merges two attribute sets
+  importedLibs = [
+    builders
+    services
+    validators
+    helpers
+    hardware
+  ];
+in
+lib.extend (_: _: foldl recursiveUpdate { } importedLibs)
