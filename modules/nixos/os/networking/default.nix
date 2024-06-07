2afc3c37fca7efe68e28062272377549799b1a03
yosoof3 <boxicalcube@proton.me>
2024-06-06 21:42:02 -0400

    refactor: restore network config

--- modules/nixos/os/networking/default.nix
+++ modules/nixos/os/networking/default.nix
@@ -6,14 +6,36 @@ let
 in
 {
   imports = [
-    # ./firewall
+    ./firewall
 
-    # ./blocker.nix
+    ./blocker.nix
     ./networkmanager.nix
-    # ./optimize.nix
-    # ./tailscale.nix
-    # ./tcpcrypt.nix
+    ./optimize.nix
+    ./tailscale.nix
+    ./tcpcrypt.nix
   ];
 
-  networking.useDHCP = mkDefault true;
+  networking = {
+    # generate a host ID by hashing the hostname
+    hostId = builtins.substring 0 8 (builtins.hashString "md5" config.networking.hostName);
+
+    hostName = config.modules.system.hostname;
+    # global dhcp has been deprecated upstream, so we use networkd instead
+    # however individual interfaces are still managed through dhcp in hardware configurations
+    useDHCP = mkForce true;
+    useNetworkd = mkForce false;
+
+    # interfaces are assigned names that contain topology information (e.g. wlp3s0) and thus should be consistent across reboots
+    # this already defaults to true, we set it in case it changes upstream
+    usePredictableInterfaceNames = mkDefault true;
+
+    # dns
+    nameservers = mkIf (dev.type != "wsl") [
+      "1.1.1.1"
+      "1.0.0.1"
+      "9.9.9.9"
+    ];
+
+    enableIPv6 = true;
+  };
 }
