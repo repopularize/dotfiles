2ee0019b2a889158889ae9019a32fc32845c64c0
zramctl <zerowatts@tuta.io>
2024-12-23 20:02:18 -0500

    refactor: :sparkles: new install

--- hosts/wattage/hardware.nix
+++ hosts/wattage/hardware.nix
@@ -1,6 +1,6 @@
 {
   fileSystems."/" = {
-    device = "/dev/disk/by-uuid/e9902caf-9c25-4316-84a9-636b52b0d046";
+    device = "/dev/disk/by-uuid/de1b271b-7b49-4e1c-8110-b7eceba0cb3e";
     fsType = "ext4";
   };
 
